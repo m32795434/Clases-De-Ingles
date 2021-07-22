f8fe073c7b012bbeaa9adc3c8a58b189ac63c367
m32795434 <manubravard@gmail.com>
2021-07-21 23:49:02 -0300

    creamos pages and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {ContactComponent} from './pages/contact/contact.component';
+import {AboutComponent} from './pages/about/about.component';
+import {ClassesComponent} from './pages/classes/classes.component';
+import {ResourcesComponent} from './pages/resources/resources.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'classes', component: ClassesComponent},
+  { path: 'resources', component: ResourcesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ClassesComponent } from './pages/classes/classes.component';
+import { ResourcesComponent } from './pages/resources/resources.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    AboutComponent,
+    ClassesComponent,
+    ResourcesComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
