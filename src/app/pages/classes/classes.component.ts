e9baccb565c2c1979a4f49a836357326ce6ab073
m32795434 <manubravard@gmail.com>
2021-07-30 00:12:38 -0300

    render classes.component

--- src/app/pages/classes/classes.component.ts
+++ src/app/pages/classes/classes.component.ts
@@ -12,27 +12,39 @@ export class ClassesComponent implements OnInit {
 blocks=[
   {
     title:"Exámenes Internacionales",
-    text: "Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y del insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre"
+    text: "Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y del insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre",
+    src:"/assets/images/international.jpg",
+    alt:"Exámenes Internacionales"
   },
   {
     title:"Clases de Apoyo Escolar",
-    text:"Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y del insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre."
+    text:"Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y del insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre.",
+    src:"/assets/images/escolar.jpg",
+    alt:"Clases de Apoyo Escolar"
   },
   {
     title:"Apoyo a Alumnos del Profesorado",
-    text:"Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y del insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre."
+    text:"Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y del insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre.",
+    src:"/assets/images/profesorado.jpg",
+    alt:"Profesorado"
   },
   {
     title:"Clases de Conversación",
-    text:"Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y del insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre."
+    text:"Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y del insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre.",
+    src:"/assets/images/conversacion.jpg",
+    alt:"Clases de Conversación"
   },
   {
     title:"Inglés con Fines Específicos",
-    text:"Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y del insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre."
+    text:"Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y del insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre.",
+    src:"/assets/images/ingeniero.jpg",
+    alt:"Inglés con Fines Específicos"
   },
   {
     title:"Inglés General",
-    text:"Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y del insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre."
+    text:"Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y del insoportable primo Dudley. Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre.",
+    src:"/assets/images/general.jpg",
+    alt:"Inglés General"
   }
 ]
 
