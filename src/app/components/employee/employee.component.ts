16422f003825573dafefa151a1aac9e0fdb1d0e7
m32795434 <manubravard@gmail.com>
2021-08-10 00:52:29 -0300

    testing bootswatch and employee service/component

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from "../../services/employee.service";
 
 @Component({
   selector: 'app-employee',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private employeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.employeService.getEmployees()
+    {}
   }
-
 }
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+URL_API = 'http://localhost:3000/api/employees';
+
+  constructor(private http:HttpClient ) { }
+
+  getEmployees() {
+    return this.http.get(this.URL_API);
+  }
 }
