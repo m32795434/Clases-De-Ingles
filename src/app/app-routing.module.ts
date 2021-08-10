70a2c4f536b109d58ebd8ce4716e6b5e2f2892fe
m32795434 <manubravard@gmail.com>
2021-08-10 01:44:48 -0300

    employee path created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import {ContactComponent} from './pages/contact/contact.component';
 import {AboutComponent} from './pages/about/about.component';
 import {ClassesComponent} from './pages/classes/classes.component';
 import {ResourcesComponent} from './pages/resources/resources.component';
+import {EmployeeComponent} from './components/employee/employee.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'contact', component: ContactComponent},
   { path: 'about', component: AboutComponent},
   { path: 'classes', component: ClassesComponent},
-  { path: 'resources', component: ResourcesComponent}
+  { path: 'resources', component: ResourcesComponent},
+  { path: 'employee', component: EmployeeComponent}
 ];
 
 @NgModule({
