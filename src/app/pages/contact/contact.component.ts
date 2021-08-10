c510baeb7f2ca7eb41e45a1735d0c3d7d943abf9
m32795434 <manubravard@gmail.com>
2021-08-10 20:15:06 -0300

    addContacto created

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactoService } from "../../services/contacto.service";
+import {NgForm} from '@angular/forms'
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
-  
+  constructor(public contactoService: ContactoService) { }
+
   ngOnInit(): void {
   }
+
+  addContacto(form: NgForm){
+  
+    this.contactoService.createContacto(form.value).subscribe(
+          res => {
+            console.log(res);
+            form.reset();
+          },
+          err => console.error(err)
+        )}
 
 }
